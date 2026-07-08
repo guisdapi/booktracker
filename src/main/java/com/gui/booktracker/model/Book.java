45f68676f54c4b3b2470b96029388b2f8b123fea
Guilherme <guilherme.s.dapi@gmail.com>
2026-07-07 21:14:57 -0300

    First commit: Estrutura inicial do BookTracker com Spring Boot

---
+++ src/main/java/com/gui/booktracker/model/Book.java
@@ -0,0 +1,61 @@
+package com.gui.booktracker.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String titulo;
+    private String autor;
+    private int totalPaginas;
+    private int paginasLidas;
+
+    public Book() {
+    }
+
+    public int getPaginasLidas() {
+        return paginasLidas;
+    }
+
+    public void setPaginasLidas(int paginasLidas) {
+        this.paginasLidas = paginasLidas;
+    }
+
+    public int getTotalPaginas() {
+        return totalPaginas;
+    }
+
+    public void setTotalPaginas(int totalPaginas) {
+        this.totalPaginas = totalPaginas;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
